1110aeed98b9437e79f04ad6b717008536552755
briri <brian.riley@ucop.edu>
2024-04-10 08:30:46 -0700

    updated data migration script to fetch db info from ssm

--- data-migrations/process.sh
+++ data-migrations/process.sh
@@ -1,10 +1,11 @@
-SECRET_RESPONSE=$(aws secretsmanager get-secret-value --secret-id ${SECRETS_MANAGER_ARN})
+# SECRET_RESPONSE=$(aws secretsmanager get-secret-value --secret-id ${SECRETS_MANAGER_ARN})
 
-HOST=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .host)
-PORT=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .port)
-DB=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .dbname)
-USER=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .username)
-PASSWORD=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .password)
+# HOST=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .host)
+HOST=$(echo aws ssm get-parameter --name /uc3/dmp/hub/dev/DbHost | jq -r .Parameter.Value )
+PORT=$(echo aws ssm get-parameter --name /uc3/dmp/hub/dev/DbPort | jq -r .Parameter.Value )
+DB=$(echo aws ssm get-parameter --name /uc3/dmp/hub/dev/DbName | jq -r .Parameter.Value )
+USER=$(echo aws ssm get-parameter --name /uc3/dmp/hub/dev/DbUsername | jq -r .Parameter.Value )
+PASSWORD=$(echo aws ssm get-parameter --name /uc3/dmp/hub/dev/DbPassword | jq -r .Parameter.Value )
 
 for i in *.sql; do
   [ -f "$i" ] || break
