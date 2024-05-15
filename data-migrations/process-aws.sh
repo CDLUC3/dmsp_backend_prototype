ea378dcaa59991fb557435799f54e852a4a98b8b
briri <brian.riley@ucop.edu>
2024-05-15 16:42:23 -0700

    updated data-migration script for AWS so that it uses RDS params passed into CodeBuild

--- data-migrations/process-aws.sh
+++ data-migrations/process-aws.sh
@@ -6,17 +6,9 @@
 # SECRET_RESPONSE=$(aws secretsmanager get-secret-value --secret-id ${SECRETS_MANAGER_ARN})
 # HOST=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .host)
 
-HOST=$(aws ssm get-parameter --name /uc3/dmp/hub/$1/DbHost | jq -r .Parameter.Value)
-PORT=$(aws ssm get-parameter --name /uc3/dmp/hub/$1/DbPort | jq -r .Parameter.Value)
-DB=$(aws ssm get-parameter --name /uc3/dmp/hub/$1/DbName | jq -r .Parameter.Value)
-USER=$(aws ssm get-parameter --name /uc3/dmp/hub/$1/DbUsername | jq -r .Parameter.Value)
-PASSWORD=$(aws ssm get-parameter --name /uc3/dmp/hub/$1/DbPassword | jq -r .Parameter.Value)
-
 # This is intended to be run during the AWS CodeBuild process to migrate new database changes
 for i in *.sql; do
   [ -f "$i" ] || break
-  echo "Found a migration file: ${i} ..."
-  echo "mysql -h ${HOST} -p ${PORT} -u ${USER} -p ${PASSWORD} ${DB} < ${i}"
-  echo $(which mysql)
-  mysql -h ${HOST} -p ${PORT} -u ${USER} -u ${PASSWORD} ${DB} < ${i}
+  echo "Found a database migration file: ${i} ..."
+  mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < $1
 done
