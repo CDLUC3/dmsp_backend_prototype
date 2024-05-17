04a55d65ee8024c202ae615c4a145b9b249cec5a
briri <brian.riley@ucop.edu>
2024-05-17 12:05:42 -0700

    fixed broken tests

--- test/setup.ts
+++ test/setup.ts
@@ -2,13 +2,13 @@ import { ApolloServer } from '@apollo/server';
 import { addMocksToSchema } from '@graphql-tools/mock';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
-import { DataSourceContext } from '../src/context';
+import { MyContext } from '../src/context';
 import { typeDefs } from '../src/schema';
 import { resolvers } from '../src/resolver';
 import { mocks } from '../src/mocks';
 
 // Test server using mocks
-export const server = new ApolloServer<DataSourceContext>({
+export const server = new ApolloServer<MyContext>({
   schema: addMocksToSchema({
     schema: makeExecutableSchema({ typeDefs, resolvers }),
     mocks,
