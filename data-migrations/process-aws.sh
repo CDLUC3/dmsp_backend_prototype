f05096db33b8b91fc0bbb91db1b2214575e4fa97
briri <brian.riley@ucop.edu>
2024-05-21 15:41:23 -0700

    updated process-aws.sh data migration script so that it can be run from Cloud9

--- data-migrations/process-aws.sh
+++ data-migrations/process-aws.sh
@@ -6,6 +6,17 @@
 # SECRET_RESPONSE=$(aws secretsmanager get-secret-value --secret-id ${SECRETS_MANAGER_ARN})
 # HOST=$(echo $SECRET_RESPONSE | jq '.SecretString | fromjson' | jq -r .host)
 
+if [ $# -ne 1 ]; then
+  echo 'Wrong number of arguments. Expecting 1: the env (e.g. `./process-aws.sh dev`)'
+  exit 1
+fi
+
+MYSQL_HOST=$(echo `aws ssm get-parameter --name /uc3/dmp/hub/${1}/DbHost | jq .Parameter.Value | sed -e "s/\"//g"`)
+MYSQL_PORT=$(echo `aws ssm get-parameter --name /uc3/dmp/hub/${1}/DbPort | jq .Parameter.Value | sed -e "s/\"//g"`)
+MYSQL_DATABASE=$(echo `aws ssm get-parameter --name /uc3/dmp/hub/${1}/DbName | jq .Parameter.Value | sed -e "s/\"//g"`)
+MYSQL_USER=$(echo `aws ssm get-parameter --name /uc3/dmp/hub/${1}/DbUsername | jq .Parameter.Value | sed -e "s/\"//g"`)
+MYSQL_PASSWORD=$(echo `aws ssm get-parameter --name /uc3/dmp/hub/${1}/DbPassword | jq .Parameter.Value | sed -e "s/\"//g"`)
+
 # This is intended to be run during the AWS CodeBuild process to migrate new database changes
 for i in *.sql; do
   [ -f "$i" ] || break
