6036bb734a49b79f611305cc0478a295bbe1562e
briri <brian.riley@ucop.edu>
2024-09-13 11:39:43 -0700

    removed commented oout code

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -18,17 +18,3 @@ jest.mock('../config/oauthConfig', () => ({
     refreshTokenLifetime: 30,
   }
 }))
-
-// Mock the MySQL connection
-/*
-jest.mock('../datasources/mySQLDataSource', () => {
-  return {
-    __esModule: true,
-    MySQLDataSource: {
-      getInstance: jest.fn().mockReturnValue({
-        query: jest.fn(),
-      }),
-    },
-  };
-});
-*/
