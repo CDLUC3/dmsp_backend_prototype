c8cafeca76a7a09776bc11d7f30eccfdd6138a89
Juliet Shin <juliet.shin@ucop.edu>
2024-09-30 11:53:45 -0700

    added comment in corsConfig.ts

--- src/config/corsConfig.ts
+++ src/config/corsConfig.ts
@@ -5,6 +5,7 @@ const corsConfig = () => {
 
   const isProduction = process.env.NODE_ENV === 'production';
 
+  //TBD: Need to update later to handle different origins for stage and production
   const corsOptions = {
     origin: isProduction ? 'TBD' : 'http://localhost:3000', //frontend
     credentials: true,               // Enable credentials (cookies, authorization headers)
