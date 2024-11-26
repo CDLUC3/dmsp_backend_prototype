22c751566457800383bd631fa6ef7fd0274a16b0
briri <brian.riley@ucop.edu>
2024-11-26 08:58:45 -0800

    temp coment out auth check on email tester due to issue with cache

--- src/controllers/emailTestController.ts
+++ src/controllers/emailTestController.ts
@@ -13,19 +13,19 @@ export const emailTestController = async (req: Request, res: Response) => {
   try {
     // For some reason req.auth is `undefined` here even though authMiddleware is called.
     // It's in req.cookies though :/
-    const accessToken = req.cookies?.dmspt;
+    //const accessToken = req.cookies?.dmspt;
 
-    if (accessToken) {
-      const token = verifyAccessToken(accessToken);
+    //if (accessToken) {
+      //const token = verifyAccessToken(accessToken);
 
       // Only allow this for Super Admins!
-      if (token && token.jti && isSuperAdmin(token as JWTAccessToken)) {
+      // if (token && token.jti && isSuperAdmin(token as JWTAccessToken)) {
         sendTestEmailNotification();
         res.status(200).json({ message: 'Successfully sent test email message to the helpdesk address.' });
-      }
-    } else {
-      res.status(401).json({ error: 'Unauthorized' });
-    }
+      //}
+    //} else {
+    //  res.status(401).json({ error: 'Unauthorized' });
+    //}
   } catch (err) {
     formatLogMessage(logger).error(err, 'Email Test error!');
     res.status(500).json({ error: 'An unexpected error occurred' });
