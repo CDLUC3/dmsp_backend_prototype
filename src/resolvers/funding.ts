b75576178602205f18a1fb0a3ca268966cd73461
sfisher <scott.fisher@ucop.edu>
2025-09-11 13:20:39 -0700

    wording chagne

--- src/resolvers/funding.ts
+++ src/resolvers/funding.ts
@@ -232,7 +232,7 @@ export const resolvers: Resolvers = {
           throw context?.token ? ForbiddenError() : AuthenticationError();
         }
 
-        // todo: remove this once I see the map version is working
+        // todo: remove this once I see the promise/map version is working
         // const newFundings = [];
         // for(const projectFundingId of pfids) {
         //   const newFunding = new PlanFunding({planId, projectFundingId});
