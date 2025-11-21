e4dcb06844324540e88de3c98dfbcd1bc8c51afc
briri <brian.riley@ucop.edu>
2025-11-21 07:34:09 -0800

    update stored proc sql migration to use utf8mb4_0900_ai_ci

--- data-migrations/2025-11-19-1427-init-stored-procs.sql
+++ data-migrations/2025-11-19-1427-init-stored-procs.sql
@@ -22,7 +22,7 @@ BEGIN
     `sourceUrl`        VARCHAR(255) NOT NULL
   ) ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    COLLATE = utf8mb4_0900_ai_ci;
 
   DROP TEMPORARY TABLE IF EXISTS stagingRelatedWorks;
   CREATE TEMPORARY TABLE stagingRelatedWorks
@@ -45,7 +45,7 @@ BEGIN
     CONSTRAINT unique_hash UNIQUE (`dmpDoi`, `workDoi`)
   ) ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    COLLATE = utf8mb4_0900_ai_ci;
 END$$
 
 DELIMITER ;
@@ -102,7 +102,7 @@ DROP TEMPORARY TABLE IF EXISTS resolvedStagingLinks;
   )
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+    COLLATE = utf8mb4_0900_ai_ci;
 
 INSERT INTO resolvedStagingLinks (id, planId, workVersionId, workDoi)
 SELECT s.id  AS id,
