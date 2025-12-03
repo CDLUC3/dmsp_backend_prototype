8588ef66dcbaee9bd16437ea10e916be94a16afb
Juliet Shin <juliet.shin@ucop.edu>
2025-12-03 08:25:49 -0800

    changed description field for guidance group to VARCHAR(300)

--- data-migrations/2025-11-24-update-to-guidanceGroups.sql
+++ data-migrations/2025-11-24-update-to-guidanceGroups.sql
@@ -1,7 +1,7 @@
 -- Add description field to guidanceGroup table
 ALTER TABLE `guidanceGroups`
-ADD COLUMN `description` VARCHAR(2000) NOT NULL DEFAULT '' AFTER `name`;
+ADD COLUMN `description` VARCHAR(300) NOT NULL DEFAULT '' AFTER `name`;
 
 -- Add description field to versionedGuidanceGroup table
 ALTER TABLE `versionedGuidanceGroups`
-ADD COLUMN `description` VARCHAR(2000) NOT NULL DEFAULT '' AFTER `name`;
+ADD COLUMN `description` VARCHAR(300) NOT NULL DEFAULT '' AFTER `name`;
